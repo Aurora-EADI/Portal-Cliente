9c14696eb1884a4b91577fa06ebf44a205a56099
JonathanF439 <jonathan.ferreira@auroraeadi.com.br>
2026-01-09 14:08:19 -0400

    refactor: Add X-API-Key

--- aurora-eadi-front/src/lib/api.ts
+++ aurora-eadi-front/src/lib/api.ts
@@ -11,6 +11,7 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333/api',
   headers: {
     'Content-Type': 'application/json',
+    ...(process.env.NEXT_PUBLIC_API_KEY ? { 'X-API-Key': process.env.NEXT_PUBLIC_API_KEY } : {}),
   },
 });
 
