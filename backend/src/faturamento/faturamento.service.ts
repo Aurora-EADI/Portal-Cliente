cb400c53d3eeea556a49e428fd9d7779a2a7f73d
JonathanF439 <jonathan.ferreira@auroraeadi.com.br>
2026-01-27 08:19:13 -0400

    refactor: Consulta faturamento detalhado

--- backend/src/faturamento/faturamento.service.ts
+++ backend/src/faturamento/faturamento.service.ts
@@ -28,27 +28,25 @@ export class FaturamentoService {
     const inicio = toSqlString(dataInicio);
     const fim = toSqlString(dataFim);
 
-    const query = `
-      SELECT *
-      FROM dbo.fnConsulta_Faturamento_Por_Periodo(@param1, @param2) AS a
-    `;
-
-
-
     // const query = `
-    //         SELECT *
-    //   FROM dbo.fnConsulta_Faturamento_Por_Periodo(
-    //       @param1,
-    //       @param2
-    //   )
-    //   WHERE NOT (
-    //       dt_entrada IS NULL
-    //       AND CAST(
-    //           REPLACE(REPLACE(valor_cif, '.', ''), ',', '.') 
-    //           AS DECIMAL(18,2)
-    //       ) = 0
-    //   );
+    //   SELECT *
+    //   FROM dbo.fnConsulta_Faturamento_Por_Periodo(@param1, @param2) AS a
     // `;
+
+    const query = `
+            SELECT *
+      FROM dbo.fnConsulta_Faturamento_Por_Periodo(
+          @param1,
+          @param2
+      )
+      WHERE NOT (
+          dt_entrada IS NULL
+          AND CAST(
+              REPLACE(REPLACE(valor_cif, '.', ''), ',', '.') 
+              AS DECIMAL(18,2)
+          ) = 0
+      );
+    `;
 
     return this.sqlServer.query<TypeDetailedBilling>(query, [inicio, fim]);
   }
